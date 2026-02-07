bb1b73518da29d314ad56bcd0c633c77b44770e0
sanchay12-art <sanchay.dutt_mca24@gla.ac.in>
2026-02-08 02:56:35 +0530

    added gitignore

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const http = require("http");
-const socketIO = require("socket.io");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = new Server(server);
 
-app.use(express.static("public"));
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
 
+// Socket connection
 io.on("connection", (socket) => {
   console.log("User connected");
 
   socket.on("sendMessage", (data) => {
-    io.emit("receiveMessage", data);
+    io.emit("receiveMessage", data); // sabko message bhejega
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// ❗ VERY IMPORTANT FOR RENDER
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
