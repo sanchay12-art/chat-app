03277d61724734f6a60ac2bf442b0cf64ffec56f
sanchay12-art <rram3367@gmail.com>
2026-02-08 05:58:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,61 @@
 const express = require("express");
 const http = require("http");
+const fs = require("fs");
 const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+app.use(express.json());
 app.use(express.static("public"));
+
+const DB_FILE = "chats.json";
+
+// load db
+let db = fs.existsSync(DB_FILE)
+  ? JSON.parse(fs.readFileSync(DB_FILE))
+  : {};
+
+function saveDB() {
+  fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
+}
+
+// get chats list
+app.post("/getChats", (req, res) => {
+  const { userId, password } = req.body;
+  const key = userId + "_" + password;
+  res.json(db[key] || {});
+});
 
 io.on("connection", (socket) => {
 
-  // JOIN ROOM USING USERID + PASSWORD
-  socket.on("joinRoom", ({ userId, password }) => {
-    const room = userId + "_" + password;
+  socket.on("joinRoom", ({ userId, password, friend }) => {
+    const acc = userId + "_" + password;
+    const room = acc + "_" + friend;
+
     socket.join(room);
+
+    if (!db[acc]) db[acc] = {};
+    if (!db[acc][friend]) db[acc][friend] = [];
+
+    saveDB();
   });
 
-  // SEND MESSAGE (ROOM ONLY)
   socket.on("sendMessage", (data) => {
-    data.id = Date.now();
-    const room = data.userId + "_" + data.password;
-    io.to(room).emit("receiveMessage", data);
-  });
+    const acc = data.userId + "_" + data.password;
+    const room = acc + "_" + data.friend;
 
-  // SEEN EVENT
-  socket.on("messageSeen", (data) => {
-    const room = data.userId + "_" + data.password;
-    io.to(room).emit("messageSeenUpdate", data.msgId);
+    const msg = {
+      id: Date.now(),
+      user: data.userId,
+      text: data.message
+    };
+
+    db[acc][data.friend].push(msg);
+    saveDB();
+
+    io.to(room).emit("receiveMessage", msg);
   });
 
 });
