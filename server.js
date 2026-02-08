f0492ebaf3299366b7cd5596c0e4e908abfc4167
sanchay12-art <rram3367@gmail.com>
2026-02-08 06:08:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,44 +7,33 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.use(express.json());
 app.use(express.static("public"));
 
-const DB_FILE = "chats.json";
+const DB_FILE = "chat-db.json";
 
-// load db
-let db = fs.existsSync(DB_FILE)
+// load old chats
+let chats = fs.existsSync(DB_FILE)
   ? JSON.parse(fs.readFileSync(DB_FILE))
   : {};
 
 function saveDB() {
-  fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
+  fs.writeFileSync(DB_FILE, JSON.stringify(chats, null, 2));
 }
-
-// get chats list
-app.post("/getChats", (req, res) => {
-  const { userId, password } = req.body;
-  const key = userId + "_" + password;
-  res.json(db[key] || {});
-});
 
 io.on("connection", (socket) => {
 
-  socket.on("joinRoom", ({ userId, password, friend }) => {
-    const acc = userId + "_" + password;
-    const room = acc + "_" + friend;
-
+  // JOIN ROOM (ID + PASSWORD = ROOM)
+  socket.on("joinRoom", ({ userId, password }) => {
+    const room = userId + "_" + password;
     socket.join(room);
 
-    if (!db[acc]) db[acc] = {};
-    if (!db[acc][friend]) db[acc][friend] = [];
-
-    saveDB();
+    if (!chats[room]) chats[room] = [];
+    socket.emit("loadOldMessages", chats[room]);
   });
 
+  // SEND MESSAGE
   socket.on("sendMessage", (data) => {
-    const acc = data.userId + "_" + data.password;
-    const room = acc + "_" + data.friend;
+    const room = data.userId + "_" + data.password;
 
     const msg = {
       id: Date.now(),
@@ -52,7 +41,7 @@ io.on("connection", (socket) => {
       text: data.message
     };
 
-    db[acc][data.friend].push(msg);
+    chats[room].push(msg);
     saveDB();
 
     io.to(room).emit("receiveMessage", msg);
