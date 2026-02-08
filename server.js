749dc876d4dd7af3c2304ea33afac6d39622bb28
sanchay12-art <rram3367@gmail.com>
2026-02-08 06:15:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,46 +9,35 @@ const io = socketIO(server);
 
 app.use(express.static("public"));
 
-const DB_FILE = "chat-db.json";
+const DB = "chat-db.json";
+let chats = fs.existsSync(DB) ? JSON.parse(fs.readFileSync(DB)) : {};
 
-// load old chats
-let chats = fs.existsSync(DB_FILE)
-  ? JSON.parse(fs.readFileSync(DB_FILE))
-  : {};
-
-function saveDB() {
-  fs.writeFileSync(DB_FILE, JSON.stringify(chats, null, 2));
+function saveDB(){
+  fs.writeFileSync(DB, JSON.stringify(chats, null, 2));
 }
 
 io.on("connection", (socket) => {
 
-  // JOIN ROOM (ID + PASSWORD = ROOM)
-  socket.on("joinRoom", ({ userId, password }) => {
-    const room = userId + "_" + password;
+  socket.on("joinRoom", ({ room }) => {
     socket.join(room);
-
-    if (!chats[room]) chats[room] = [];
+    if(!chats[room]) chats[room] = [];
     socket.emit("loadOldMessages", chats[room]);
   });
 
-  // SEND MESSAGE
   socket.on("sendMessage", (data) => {
-    const room = data.userId + "_" + data.password;
-
     const msg = {
       id: Date.now(),
-      user: data.userId,
+      user: data.user,      // sender name
       text: data.message
     };
 
-    chats[room].push(msg);
+    chats[data.room].push(msg);
     saveDB();
 
-    io.to(room).emit("receiveMessage", msg);
+    io.to(data.room).emit("receiveMessage", msg);
   });
 
 });
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+server.listen(3000, () =>
+  console.log("Server running on port 3000")
+);
