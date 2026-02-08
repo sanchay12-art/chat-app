3525651d8d6b7dc69d20c412db43b7a71738605e
sanchay12-art <rram3367@gmail.com>
2026-02-08 05:47:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,30 +10,23 @@ app.use(express.static("public"));
 
 io.on("connection", (socket) => {
 
-  // JOIN PRIVATE ROOM
-  socket.on("joinRoom", ({ user, friend }) => {
-    const room =
-      user < friend ? `${user}_${friend}` : `${friend}_${user}`;
+  // JOIN ROOM USING USERID + PASSWORD
+  socket.on("joinRoom", ({ userId, password }) => {
+    const room = userId + "_" + password;
     socket.join(room);
   });
 
-  // SEND MESSAGE TO ROOM ONLY
+  // SEND MESSAGE (ROOM ONLY)
   socket.on("sendMessage", (data) => {
     data.id = Date.now();
-
-    const room =
-      data.user < data.friend
-        ? `${data.user}_${data.friend}`
-        : `${data.friend}_${data.user}`;
-
+    const room = data.userId + "_" + data.password;
     io.to(room).emit("receiveMessage", data);
   });
 
-  // SEEN EVENT (ROOM ONLY)
-  socket.on("messageSeen", ({ msgId, user, friend }) => {
-    const room =
-      user < friend ? `${user}_${friend}` : `${friend}_${user}`;
-    io.to(room).emit("messageSeenUpdate", msgId);
+  // SEEN EVENT
+  socket.on("messageSeen", (data) => {
+    const room = data.userId + "_" + data.password;
+    io.to(room).emit("messageSeenUpdate", data.msgId);
   });
 
 });
